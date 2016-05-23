15e291514bf9e1607d9788af6f024289c9cd505d
nwshane <django09@gmail.com>
2016-05-23 11:16:00 +0400

    Using data attribute on change time period select to pass in base url for new destination url.

--- app/assets/javascripts/change_time_period.js
+++ app/assets/javascripts/change_time_period.js
@@ -2,9 +2,7 @@ function setupTimePeriodSelect() {
   $('.js-change-time-period').on('change', function(e) {
     var newTimePeriod = $(e.target).val();
     var currentURL = window.location.href;
-    var oldTimePeriod = currentURL.substring(currentURL.lastIndexOf('/') + 1,
-                                             currentURL.length);
-    var newURL = currentURL.replace(oldTimePeriod, newTimePeriod);
+    var newURL = $(this).data('baseUrl') + '/' + newTimePeriod;
 
     window.location.href = newURL;
   });
