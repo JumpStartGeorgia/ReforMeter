0bcc4b5d05356bb5a59dff7ca7b60ffb96dd34b4
nwshane <django09@gmail.com>
2016-05-23 11:19:24 +0400

    Disabled bullet.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.after_initialize do
-    Bullet.enable = true
+    Bullet.enable = false
     Bullet.alert = true
     Bullet.bullet_logger = true
     Bullet.console = true
