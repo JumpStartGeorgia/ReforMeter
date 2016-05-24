9e7554dcc16249812b043c173d26a636c45b751f
nwshane <django09@gmail.com>
2016-05-24 12:09:07 +0400

    Moved overall_meter_gauge to separate file.

--- app/assets/javascripts/expert_meter_gauges.js
+++ app/assets/javascripts/expert_meter_gauges.js
@@ -1,86 +1,5 @@
 function setupExpertPage() {
-  overall_meter_gauge = {
-
-    title: {
-      text: 'Overall',
-      y: 15
-    },
-
-    pane: {
-      size: '200'
-    },
-
-    yAxis: {
-      plotBands: [
-        {
-          borderWidth: 2,
-          borderColor: 'white',
-          from: 0,
-          to: 3.3,
-          color: '#5AD7F9',
-          innerRadius: '40%',
-          outerRadius: '100%',
-          label: {
-            text: 'Poor',
-            rotation: -60,
-            x: 60,
-            y: 55,
-            style: {
-              fontSize: '18px',
-              color: 'white'
-            }
-          }
-        },{
-        	borderWidth: 2,
-          borderColor: 'white',
-          from: 3.3,
-          to: 6.6,
-          color: '#2DB9EA',
-          innerRadius: '40%',
-          outerRadius: '100%',
-          label: {
-          	text: 'Normal',
-            x: 98,
-            y: 30,
-            style: {
-              fontSize: '18px',
-              color: 'white'
-            }
-          }
-        },{
-        	borderWidth: 2,
-          borderColor: 'white',
-          from: 6.6,
-          to: 10,
-          color: '#1599D6',
-          innerRadius: '40%',
-          outerRadius: '100%',
-          label: {
-          	text: 'Good',
-            rotation: 60,
-            x: 170,
-            y: 45,
-            style: {
-              fontSize: '18px',
-              color: 'white'
-            }
-          }
-        }
-      ]
-    },
-
-    series: [{
-      name: 'Overall',
-      data: [gon.charts.performance.score],
-      dataLabels: {
-        borderWidth: 0,
-        y: 70,
-        useHTML: true,
-        format: '<div style="text-align:center;"><span style="font-size:35px;color:black;">{y:.2f}</span>' + gon.charts.overall.icon + '</div>'
-      }
-    }]
-
-  };
+  createExpertOverallMeterGauge();
 
   performance_meter_gauge = {
 
@@ -136,7 +55,7 @@ function setupExpertPage() {
 
   };
 
-  $('.js-become-expert-overall-chart').highcharts(Highcharts.merge(highchartsMeterGauge(), overall_meter_gauge));
+
 
   $('.js-become-performance-overall-chart').highcharts(Highcharts.merge(highchartsMeterGauge(), performance_meter_gauge));
 
