1200eb05561d493796321658d359c382908373d8
nwshane <django09@gmail.com>
2016-05-25 12:04:31 +0400

    Refactored tabs javascript.

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -1,17 +1,26 @@
 function setupTabs() {
-  $('.js-act-as-tab-list-button').click(function() {
-    var $selectedTabListButton = $(this);
+  function unselectSiblingTabListButtons($selectedTabListButton) {
     var $tabList = $selectedTabListButton.parents('.js-act-as-tab-list');
     var $tabListButtons = $tabList.find('.js-act-as-tab-list-button');
 
     $tabListButtons.removeClass('is-selected');
-    $selectedTabListButton.addClass('is-selected');
+  }
 
-    var $selectedTabContentPanel = $('#' + $selectedTabListButton.data('selectsTabContentPanelId'));
+  function unselectSiblingTabContentPanels($selectedTabContentPanel) {
     var $tabContent = $selectedTabContentPanel.parents('.js-act-as-tab-content');
     var $tabContentPanels = $tabContent.find('.js-act-as-tab-content-panel');
 
     $tabContentPanels.removeClass('is-selected');
+  }
+
+  $('.js-act-as-tab-list-button').click(function() {
+    var $selectedTabListButton = $(this);
+    var $selectedTabContentPanel = $('#' + $selectedTabListButton.data('selectsTabContentPanelId'));
+
+    unselectSiblingTabListButtons($selectedTabListButton);
+    unselectSiblingTabContentPanels($selectedTabContentPanel);
+    
+    $selectedTabListButton.addClass('is-selected');
     $selectedTabContentPanel.addClass('is-selected');
   });
 }
