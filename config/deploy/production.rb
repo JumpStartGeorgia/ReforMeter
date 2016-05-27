38be941ccc2716f07ac8fac3dd3e3424034bcbdb
jason <jason.addie@gmail.com>
2016-05-27 15:59:29 +0400

    added comment to deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,6 @@
 set :domain, 'alpha.jumpstart.ge'
 set :user, 'prisoners-staging'
 set :application, 'Starter-Template-Production'
+# easier to use https; if you use ssh then you have to create key on server
 set :repository, 'git@github.com:JumpStartGeorgia/Starter-Template.git'
 set :web_url, ENV['PRODUCTION_WEB_URL']
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,7 @@
 set :domain, 'alpha.jumpstart.ge'
 set :user, 'reformeter-staging'
 set :application, 'ReforMeter-Staging'
+# easier to use https; if you use ssh then you have to create key on server
 set :repository, 'https://github.com/JumpStartGeorgia/ReforMeter.git'
 set :web_url, ENV['STAGING_WEB_URL']
 set :visible_to_robots, false
