f81386b07ad3ceccac6adabc1571aa4a6999bd9e
jason <jason.addie@gmail.com>
2016-05-27 16:25:40 +0400

    bug fix - env variable not work in staging deploy file

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,5 +3,5 @@ set :user, 'reformeter-staging'
 set :application, 'ReforMeter-Staging'
 # easier to use https; if you use ssh then you have to create key on server
 set :repository, 'https://github.com/JumpStartGeorgia/ReforMeter.git'
-set :web_url, ENV['STAGING_WEB_URL']
+set :web_url, 'dev-reformeter.jumpstart.ge'
 set :visible_to_robots, false
