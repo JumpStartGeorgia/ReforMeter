8dac0bbb5ff534e9f13b52d3377c2949c9191126
jason <jason.addie@gmail.com>
2016-05-31 15:01:45 +0400

    add branch to deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,5 @@ set :user, 'prisoners-staging'
 set :application, 'Starter-Template-Production'
 # easier to use https; if you use ssh then you have to create key on server
 set :repository, 'git@github.com:JumpStartGeorgia/Starter-Template.git'
+set :branch, 'master'
 set :web_url, ENV['PRODUCTION_WEB_URL']
