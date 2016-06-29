af4635a2625fd71b40d862567e45b0b0a34c58eb
nwshane <django09@gmail.com>
2016-06-29 10:59:47 +0400

    Fixed CKEditor to work with turbolinks restoration visits.

--- app/assets/javascripts/rm_rich_text_area.js
+++ app/assets/javascripts/rm_rich_text_area.js
@@ -4,8 +4,20 @@ var RMRichTextArea = (function() {
 
   exports.load = function() {
 
+    var loaded_instances = Object.getOwnPropertyNames(CKEDITOR.instances);
+
     $(selector).each(
       function() {
+
+        var editor_id = $(this).attr('id');
+
+        // Remove CKEditor instance if already loaded.
+        // Necessary for compatibility with Turbolinks restoration visits,
+        // in which a cached version of the page is loaded
+        if (loaded_instances.includes(editor_id)) {
+          CKEDITOR.remove(CKEDITOR.instances[editor_id]);
+        }
+
         CKEDITOR.replace($(this).attr('id'));
       }
     );
