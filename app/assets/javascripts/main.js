42d5ae42a9ce89ddd633491fc052e5bea64fa85a
nwshane <django09@gmail.com>
2016-06-29 13:58:08 +0400

    Opening up experts tab when there is an anchor asking for an expert item.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,9 +4,23 @@ $(document).ready(function() {
   setupExpertTimePeriodSelect();
   makeReformPageChangeable();
   setupCharts();
-  setupTabs();
 
   if ($("body").hasClass("root download_data_and_reports")) {
     activate_download_events();
   }
+});
+
+function openExpertsTab() {
+  // return if anchor tag does not match expert-{number}
+  var anchor_tag = window.location.hash.substring(1);
+  if (!/expert-(\d+)/.test(anchor_tag)) return false;
+
+  $('.js-act-as-tab-list-button[data-selects-tab-content-panel-id="experts"]').click();
+}
+
+$(document).on('page:change', function() {
+  setupTabs();
+
+  if ($('body.root.experts').length > 0) openExpertsTab();
+
 });
