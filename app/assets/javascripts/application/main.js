b04fdb85051829de93c6749b5370b39ebd88e93f
nwshane <django09@gmail.com>
2016-07-01 13:08:33 +0400

    Wrapped code in application/main.js in immediately invoked anonymous function.

--- app/assets/javascripts/application/main.js
+++ app/assets/javascripts/application/main.js
@@ -1,17 +1,19 @@
-$(document).ready(function() {
-  setup_navigation_menu();
-  setupSelect2();
-  setupExpertTimePeriodSelect();
-  makeReformPageChangeable();
-  setupCharts();
+(function() {
+  $(document).ready(function() {
+    setup_navigation_menu();
+    setupSelect2();
+    setupExpertTimePeriodSelect();
+    makeReformPageChangeable();
+    setupCharts();
 
-  if ($("body").hasClass("root download_data_and_reports")) {
-    activate_download_events();
-  }
-});
+    if ($("body").hasClass("root download_data_and_reports")) {
+      activate_download_events();
+    }
+  });
 
-$(document).on('page:change', function() {
-  setupTabs();
+  $(document).on('page:change', function() {
+    setupTabs();
 
-  if ($('body.root.experts').length > 0) openExpertsTab();
-});
+    if ($('body.root.experts').length > 0) openExpertsTab();
+  });
+})();
