7c124cd90c051454d3e2911a95c61f831825385f
nwshane <django09@gmail.com>
2016-07-01 14:01:54 +0400

    Replaced jumpstart logo png with svg.

--- app/assets/javascripts/application/main.js
+++ app/assets/javascripts/application/main.js
@@ -1,17 +1,27 @@
-$(document).ready(function() {
-  setup_navigation_menu();
-  setupSelect2();
-  setupExpertTimePeriodSelect();
-  makeReformPageChangeable();
-  setupCharts();
+(function() {
+  $(document).ready(function() {
+    setup_navigation_menu();
+    setupSelect2();
+    setupExpertTimePeriodSelect();
+    makeReformPageChangeable();
+    var charts = setupCharts();
 
-  if ($("body").hasClass("root download_data_and_reports")) {
-    activate_download_events();
-  }
-});
+    if ($('body.root.reforms').length > 0) {
+      var colorfulReformsTimeSeries = charts.filter(function() {
+        return $(this.highchartsObject.renderTo).data('id') === 'reforms-history-series';
+      })[0];
 
-$(document).on('page:change', function() {
-  setupTabs();
+      setupReformSelects(colorfulReformsTimeSeries);
+    }
 
-  if ($('body.root.experts').length > 0) openExpertsTab();
-});
+    if ($("body").hasClass("root download_data_and_reports")) {
+      activate_download_events();
+    }
+  });
+
+  $(document).on('page:change', function() {
+    setupTabs();
+
+    if ($('body.root.experts').length > 0) openExpertsTab();
+  });
+})();
