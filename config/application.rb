dcc7e4759bdb0012800185041545faa7bad76f8d
jason <jason.addie@gmail.com>
2016-07-18 11:09:16 +0400

    change default locale

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module StarterTemplate
       Rails.root.join('config', 'locales', '**', '*.{rb,yml}')
     ]
 
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :ka
 
     config.i18n.available_locales = [:ka, :en]
 
