68fba6ab89c4659866a59d53bef29fb5028c7282
nwshane <django09@gmail.com>
2016-08-01 08:57:18 -0400

    Uncommented JS to fade in and out loading indicator with page change.

--- app/assets/javascripts/loading_indicator.js
+++ app/assets/javascripts/loading_indicator.js
@@ -1,9 +1,9 @@
 // Shows and hides the loading indicator when users switch pages
 
-// $(document).on('page:before-change', function() {
-//   $('.js-fade-in-out-loading-indicator').fadeIn();
-// });
-//
-// $(document).on('page:change', function() {
-//   $('.js-fade-in-out-loading-indicator').fadeOut();
-// });
+$(document).on('page:before-change', function() {
+  $('.js-fade-in-out-loading-indicator').fadeIn();
+});
+
+$(document).on('page:change', function() {
+  $('.js-fade-in-out-loading-indicator').fadeOut();
+});
