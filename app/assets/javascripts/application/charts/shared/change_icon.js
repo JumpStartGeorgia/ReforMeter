138821ecd74278cf7dfc9813c1608779d3f40200
nwshane <django09@gmail.com>
2016-08-09 15:44:24 +0400

    Removed debugger line.

--- app/assets/javascripts/application/charts/shared/change_icon.js
+++ app/assets/javascripts/application/charts/shared/change_icon.js
@@ -2,10 +2,8 @@ function change_icon(change_number) {
   if (!gon.change_icons) {
     throw new Error('Change icons not available');
   }
-  
-  var icon = gon.change_icons[change_number];
 
-  if (!icon) debugger;
+  var icon = gon.change_icons[change_number];
 
   return icon;
 }
