6e1354e57b1f9d59cbf39fc15aa7a9a5cc02a449
nwshane <django09@gmail.com>
2016-08-10 13:26:13 +0400

    Configured only external indicator dropdown to have a bigger min width.

--- app/assets/javascripts/application/select2-setup.js
+++ app/assets/javascripts/application/select2-setup.js
@@ -1,6 +1,24 @@
 function setupSelect2() {
-  $('.js-become-select2').select2({
-    width: 'auto',
-    minimumResultsForSearch: -1 // removes search bar
-  });
+  function initialize() {
+    var methods = {};
+
+    var options = {
+      width: 'auto',
+      minimumResultsForSearch: -1 // removes search bar
+    };
+
+    var $select2 = $('.js-become-select2').select2(options);
+
+    methods.addClassToDropdown = function() {
+      $select2.each(function() {
+        var dropdownClass = $(this).data('dropdown-class');
+        $(this).data('select2').$dropdown.addClass(dropdownClass);
+      });
+    }
+
+    return methods;
+  }
+
+  var select2 = initialize();
+  select2.addClassToDropdown();
 }
