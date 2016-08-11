f6afeb15e74a59473fb93fafe258ce20039545ef
nwshane <django09@gmail.com>
2016-08-11 17:36:49 +0400

    Further refactor of change reform page controls code.

--- app/assets/javascripts/application/change_reform_page.js
+++ app/assets/javascripts/application/change_reform_page.js
@@ -1,10 +1,14 @@
 function initializeChangeReformPageControls() {
   var exports = {};
 
+  var $quarterSelect = $('.js-reform-page-quarter-select');
+  var $reformSelect = $('.js-reform-page-reform-select');
+  var $loadNewReformButton = $('.js-load-new-reform-page');
+
   exports.setup = function() {
-    $('.js-load-new-reform-page').click(function() {
-      var newQuarter = $('.js-reform-page-quarter-select').val();
-      var newReform = $('.js-reform-page-reform-select').val();
+    $loadNewReformButton.click(function() {
+      var newQuarter = $quarterSelect.val();
+      var newReform = $reformSelect.val();
       var newURL = $(this).data('newUrlBase') + '/' + newReform + '/' + newQuarter;
 
       window.location.href = newURL;
