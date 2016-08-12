63e49f11cf13be4718dee874617515abfdf44641
nwshane <django09@gmail.com>
2016-08-12 10:14:57 +0400

    Updating quarter select when reform select selected option changes to only allow quarters matching the selected reform.

--- app/assets/javascripts/application/change_reform_page.js
+++ app/assets/javascripts/application/change_reform_page.js
@@ -1,4 +1,5 @@
 function initializeChangeReformPageControls() {
+  var reforms_quarters = gon.linked_reforms_quarters;
   var exports = {};
 
   var $quarterSelect = $('.js-reform-page-quarter-select');
@@ -13,7 +14,40 @@ function initializeChangeReformPageControls() {
     window.location.href = newURL;
   }
 
+  function updateQuarters() {
+    var $quarterOptions = $quarterSelect.find('option');
+    var $selectedQuarter = $quarterSelect.find(':selected')
+    $quarterOptions.prop('disabled', true);
+
+    function quarterIsAllowed() {
+      var quarterOption = this;
+
+      return reforms_quarters.filter(
+        function(reformQuarterPair) {
+          var stuff = reformQuarterPair.reform_slug === $reformSelect.val() && reformQuarterPair.quarter_slug === quarterOption.value;
+          return stuff;
+        }
+      ).length > 0;
+    }
+
+    var $allowedQuarterOptions = $quarterOptions.filter(quarterIsAllowed);
+
+    $allowedQuarterOptions.prop('disabled', false);
+
+    if ($selectedQuarter.prop('disabled')) {
+      $selectedQuarter.prop('selected', false);
+      $newSelectedQuarter = $quarterSelect.find('option:enabled').first();
+      $newSelectedQuarter.prop('selected', true);
+    }
+
+    $quarterSelect.select2();
+  }
+
   exports.setup = function() {
+    $reformSelect.change(function() {
+      updateQuarters();
+    });
+
     $loadNewReformButton.click(loadNewReform);
   }
 
