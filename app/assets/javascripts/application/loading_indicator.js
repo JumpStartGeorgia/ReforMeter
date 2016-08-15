9f7368f05b35d0e0385dc01016a6c568cdd2351d
nwshane <django09@gmail.com>
2016-08-15 11:07:25 +0400

    Hiding load indicator on page load so that it disappears on turbolinks restoration visits.

--- app/assets/javascripts/application/loading_indicator.js
+++ app/assets/javascripts/application/loading_indicator.js
@@ -1,3 +1,7 @@
 $(document).on('page:before-change', function() {
   $('.js-fade-in-out-loading-indicator').fadeIn();
 });
+
+$(document).on('page:change', function() {
+  $('.js-fade-in-out-loading-indicator').css('display', 'none');
+});
