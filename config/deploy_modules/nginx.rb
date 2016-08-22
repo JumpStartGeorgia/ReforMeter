937525320ff8cd47e2ee121bcd5fbf7f4222a6bc
nwshane <django09@gmail.com>
2016-08-22 16:54:06 +0400

    Fixed nginx mina task typo.

--- config/deploy_modules/nginx.rb
+++ config/deploy_modules/nginx.rb
@@ -66,7 +66,7 @@ namespace :nginx do
   task :reload do |task|
     system %(echo "")
     system %(echo "Restarts Nginx.")
-    system %(#{sudo_ssh_cmd(task)} 'sudo nginx -s reloads')
+    system %(#{sudo_ssh_cmd(task)} 'sudo nginx -s reload')
     system %(echo "")
   end
 
