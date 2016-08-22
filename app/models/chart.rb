fe96ced835be3f8e283793fc0f61203c33c61c4a
nwshane <django09@gmail.com>
2016-08-22 16:02:44 +0400

    Changed Chart model to attach a random ID to chart image paths.

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -1,3 +1,9 @@
+# This model is primarily used for making charts shareable, i.e.
+# generating a path to an image for the chart and then providing that path
+# as necessary.
+#
+# Note: Just in case facebook (or a different social media site) caches
+# image names, the chart images are generated with a random ID in the name.
 class Chart
   def initialize(options, page_path)
     @options = options
@@ -13,25 +19,25 @@ class Chart
   end
 
   def png_image_exists?
-    File.exist?(full_png_image_path)
+    matching_image_paths.present?
   end
 
+  # If image exists, returns the image's path.
+  # Otherwise, generates a path for a new image.
   def png_image_path
-    Pathname.new(images_dir_path_for_page).join(
-      png_image_name
-    ).to_s
+    name = if (png_image_exists?)
+      File.basename(matching_image_paths[0])
+    else
+      png_image_name_new
+    end
+
+    Pathname.new(images_dir_path_for_page).join(name).to_s
   end
 
   private
 
   def remove_forward_slash(str)
     str[0] === '/' ? str.slice(1, str.length) : str
-  end
-
-  def full_png_image_path
-    Rails.public_path.join(
-      png_image_path
-    ).to_s
   end
 
   def page_path
@@ -44,7 +50,32 @@ class Chart
     ).to_s
   end
 
-  def png_image_name
-    "#{@options[:id]}.png"
+  def matching_image_paths
+    Dir.glob(
+      Rails.public_path.join(
+        images_dir_path_for_page,
+        png_image_name_glob
+      )
+    )
+  end
+
+  def png_image_name_new
+    require 'securerandom'
+
+    "#{png_image_name_base}_id-#{SecureRandom.hex}.png"
+  end
+
+  def png_image_name_glob
+    "#{png_image_name_base}_id-*.png"
+  end
+
+  def png_image_name_base
+    @options[:id]
+  end
+
+  def full_png_image_path
+    Rails.public_path.join(
+      png_image_path
+    ).to_s
   end
 end
