1892731d1fdf26bf90b6f2e0258e0b8e842acc40
nwshane <django09@gmail.com>
2016-08-23 12:25:20 +0400

    Refactored export chart group button to move center data label function to global scope.

--- app/assets/javascripts/application/charts/export_chart_group_button.js
+++ app/assets/javascripts/application/charts/export_chart_group_button.js
@@ -1,3 +1,55 @@
+function centerDataLabelForGaugeSVG(svg, highchartsObject, options) {
+  if (!options) options = {};
+  if (!options.topPadding) options.topPadding = false;
+
+  var $svg = $(svg);
+
+  var $dataLabel = $svg.find('g.highcharts-data-labels');
+
+  function translateString(x, y) {
+    return 'translate(' + x + ',' + y + ')';
+  }
+
+  function replaceTranslateXWithCenteredX(_, _, currentY) {
+    var centeredX = highchartsObject.chartWidth/2;
+    return translateString(centeredX, currentY);
+  }
+
+  var translateRegex = /translate\((-?\d+),(-?\d+)\)/;
+
+  var newDataLabelTransform = $dataLabel
+                              .attr('transform')
+                              .replace(
+                                translateRegex,
+                                replaceTranslateXWithCenteredX
+                              );
+
+  $dataLabel.attr('transform', newDataLabelTransform);
+
+  $dataLabelChild = $dataLabel.children('g');
+
+  function replaceTranslateXWithZero(_, _, currentY) {
+    return translateString('0', currentY);
+  }
+
+  $dataLabelChild.attr(
+    'transform',
+    $dataLabelChild.attr('transform').replace(
+      translateRegex,
+      replaceTranslateXWithZero
+    )
+  );
+
+  var $tspans =  $dataLabel.find('tspan');
+  $tspans.attr('x', 0)
+  $tspans.attr('text-anchor', 'middle');
+
+  if (options.topPadding) $tspans.slice(0, 1).attr('dy', '.5em');
+
+  $tspans.slice(1).attr('dy', '1.2em').removeAttr('dx');
+  return $svg[0].outerHTML;
+}
+
 function initializeExportChartGroupButton($exportButton, charts) {
   var exportType = $exportButton.data('export-type'),
       allowedTypes = ['image/png', 'image/jpeg', 'application/pdf', 'image/svg+xml'];
@@ -43,57 +95,13 @@ function initializeExportChartGroupButton($exportButton, charts) {
       svg = svg.replace('<svg', '<g transform="translate(' + xOffset + ',0)" ');
       svg = svg.replace('</svg>', '</g>');
 
-      function centerDataLabel(svg) {
-        var $svg = $(svg);
-
-        var $dataLabel = $svg.find('g.highcharts-data-labels');
-
-        function translateString(x, y) {
-          return 'translate(' + x + ',' + y + ')';
+      svg = centerDataLabelForGaugeSVG(
+        svg,
+        highchartsObject,
+        {
+          topPadding: index === 0
         }
-
-        function replaceTranslateXWithCenteredX(_, _, currentY) {
-          var centeredX = highchartsObject.chartWidth/2;
-          return translateString(centeredX, currentY);
-        }
-
-        var translateRegex = /translate\((-?\d+),(-?\d+)\)/;
-
-        var newDataLabelTransform = $dataLabel
-                                    .attr('transform')
-                                    .replace(
-                                      translateRegex,
-                                      replaceTranslateXWithCenteredX
-                                    );
-
-        $dataLabel.attr('transform', newDataLabelTransform);
-
-        $dataLabelChild = $dataLabel.children('g');
-
-        function replaceTranslateXWithZero(_, _, currentY) {
-          return translateString('0', currentY);
-        }
-
-        $dataLabelChild.attr(
-          'transform',
-          $dataLabelChild.attr('transform').replace(
-            translateRegex,
-            replaceTranslateXWithZero
-          )
-        );
-
-        var $tspans =  $dataLabel.find('tspan');
-        $tspans.attr('x', 0)
-        $tspans.attr('text-anchor', 'middle');
-
-        // add padding to the first chart
-        if (index === 0) $tspans.slice(0, 1).attr('dy', '.5em');
-
-        $tspans.slice(1).attr('dy', '1.2em').removeAttr('dx');
-        return $svg[0].outerHTML;
-      }
-
-      svg = centerDataLabel(svg);
+      );
 
       height = parseInt(Math.max(height, highchartsObject.chartHeight));
       xOffset += highchartsObject.chartWidth;
