930fcd32703a1238fb03ac242d68e2497c9a2fa8
jason <jason.addie@gmail.com>
2017-02-02 17:24:26 +0400

    change deploy branch

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@ set :user, 'reformeter-staging'
 set :application, 'ReforMeter-Staging'
 # easier to use https; if you use ssh then you have to create key on server
 set :repository, 'https://github.com/JumpStartGeorgia/ReforMeter.git'
-set :branch, 'master'
+set :branch, 'v2.0'
 set :web_url, 'dev-reformeter.jumpstart.ge'
 set :visible_to_robots, false
 set :use_ssl, true
