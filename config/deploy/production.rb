e2fae0a667449ac45e732130f1250e4f851a6414
jason <jason.addie@gmail.com>
2017-02-13 01:25:26 +0400

    adding correct domain for prod

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,5 +4,6 @@ set :application, 'Reformeter'
 # easier to use https; if you use ssh then you have to create key on server
 set :repository, 'https://github.com/JumpStartGeorgia/ReforMeter.git'
 set :branch, 'master'
-set :web_url, 'reformeter.jumpstart.ge'
+set :web_url, 'reformeter.iset-pi.ge'
+set :secondary_web_url, 'reformeter.jumpstart.ge'
 set :use_ssl, false
