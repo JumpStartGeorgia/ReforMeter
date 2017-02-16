dd6ca95d337adfc186505fff1e874f60944bf96c
nwshane <django09@gmail.com>
2017-02-16 12:41:02 +0400

    Finished accordion functionality.

--- app/assets/javascripts/application/accordions.js
+++ app/assets/javascripts/application/accordions.js
@@ -3,11 +3,27 @@ function setupAccordions() {
   var accordionToggleButtonSelector = '.js-accordion-toggle-button';
   var accordionTogglableContentSelector = '.js-accordion-togglable-content';
 
-  $(accordionToggleButtonSelector).click(function() {
+  function hideContentIfHidden() {
+    $(accordionSelector).each(function() {
+      var $accordion = $(this);
+
+      if (!$accordion.hasClass('is-expanded')) {
+        $accordion
+        .children(accordionTogglableContentSelector)
+        .hide();
+      }
+    })
+  }
+
+  function slideToggleContent() {
     $(this)
     .parents(accordionSelector)
     .toggleClass('is-expanded')
     .children(accordionTogglableContentSelector)
-    .slideToggle()
-  })
+    .slideToggle();
+  }
+
+  hideContentIfHidden();
+
+  $(accordionToggleButtonSelector).click(slideToggleContent)
 }
