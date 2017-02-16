95f0209b436a1ffee36d73b545510a077f01e10d
nwshane <django09@gmail.com>
2017-02-16 11:50:24 +0400

    Converted about page tabs to content sections and implemented basic accordion functionality.

---
+++ app/assets/javascripts/application/accordions.js
@@ -0,0 +1,11 @@
+function setupAccordions() {
+  var accordionSelector = '.js-accordion'
+  var accordionToggleButtonSelector = '.js-accordion-toggle-button'
+  var accordionTogglableContentSelector = '.js-accordion-togglable-content'
+
+  $(accordionToggleButtonSelector).click(function() {
+    $(this)
+    .parents(accordionSelector)
+    .toggleClass('is-expanded')
+  })
+}
