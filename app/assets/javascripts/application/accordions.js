d0fcb3e78fd39441be7bd9be5e19e4d947e92f8b
nwshane <django09@gmail.com>
2017-02-16 12:14:02 +0400

    Fixed accordion bug.

--- app/assets/javascripts/application/accordions.js
+++ app/assets/javascripts/application/accordions.js
@@ -1,11 +1,13 @@
 function setupAccordions() {
-  var accordionSelector = '.js-accordion'
-  var accordionToggleButtonSelector = '.js-accordion-toggle-button'
-  var accordionTogglableContentSelector = '.js-accordion-togglable-content'
+  var accordionSelector = '.js-accordion';
+  var accordionToggleButtonSelector = '.js-accordion-toggle-button';
+  var accordionTogglableContentSelector = '.js-accordion-togglable-content';
 
   $(accordionToggleButtonSelector).click(function() {
     $(this)
     .parents(accordionSelector)
     .toggleClass('is-expanded')
+    .children(accordionTogglableContentSelector)
+    .slideToggle()
   })
 }
