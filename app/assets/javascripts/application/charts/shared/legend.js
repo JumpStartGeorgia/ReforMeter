10e8b0c6256856e113c7a699b5e27ab34dde6e2a
nwshane <django09@gmail.com>
2017-02-17 13:42:43 +0400

    Increased the vertical margin between legend items.

--- app/assets/javascripts/application/charts/shared/legend.js
+++ app/assets/javascripts/application/charts/shared/legend.js
@@ -3,6 +3,7 @@ function getHighchartsLegend(chartData) {
     enabled: chartData.series.length > 1,
     align: 'right',
     symbolWidth: 40,
+    itemMarginTop: 10,
     itemStyle: {
       fontWeight: '400'
     }
