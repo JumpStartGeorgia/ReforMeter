189d6a97990984a39ce41d62d9b16fc4cbce1b17
jason <jason.addie@gmail.com>
2017-02-21 00:51:14 +0400

    getting ready to deploy to prod

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@ set :user, 'reformeter'
 set :application, 'Reformeter'
 # easier to use https; if you use ssh then you have to create key on server
 set :repository, 'https://github.com/JumpStartGeorgia/ReforMeter.git'
-set :branch, 'master'
+set :branch, 'v2.0'
 set :web_url, 'reformeter.iset-pi.ge'
 set :secondary_web_url, 'reformeter.jumpstart.ge'
 set :use_ssl, true
