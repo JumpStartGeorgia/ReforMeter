c239474494fdd938c03a53290288eb229957fdb7
jason <jason.addie@gmail.com>
2017-02-23 14:36:36 +0400

    set puma config nums

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,3 +7,6 @@ set :branch, 'v2.0'
 set :web_url, 'reformeter.iset-pi.ge'
 set :secondary_web_url, 'reformeter.jumpstart.ge'
 set :use_ssl, true
+set :puma_worker_count, '1'
+set :puma_thread_count_min, '1'
+set :puma_thread_count_max, '16'
